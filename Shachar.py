eef6e177ca3870ad4515194afaec68f932bc1a63
Shachar Waisman <shacharw@netapp.com>
2021-11-22 22:10:27 +0200

    fuck that shit

--- Shachar.py
+++ Shachar.py
@@ -1,9 +1,9 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, my name is Shachar")
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('shachar')
+    range = 50
+    count =0
+    while count < range:
+        print ("fuck that shit")
+        count+= 1
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
