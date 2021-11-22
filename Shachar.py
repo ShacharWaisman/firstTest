22cd36131ca5d8acfa7332acd48094832ae6cf56
Shachar Waisman <shacharw@netapp.com>
2021-11-22 17:50:05 +0200

    changed again to original

--- Shachar.py
+++ Shachar.py
@@ -4,6 +4,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('shacharVa')
+    print_hi('shachar')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
